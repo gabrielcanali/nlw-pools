c420c90fb47275e2e3c3506f9cb62dba09477903
Gabriel Canali <gabrielcanali.dev@gmail.com>
2024-02-06 22:07:47 -0300

    add: NLW Expert - Node.js Day 1

---
+++ prisma/migrations/20240207003355_create_poll/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Poll" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Poll_pkey" PRIMARY KEY ("id")
+);
